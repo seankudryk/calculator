804b386b02e880821e8c81ad0e4617e3824a154c
seankudryk <seankudryk@hotmail.ca>
2024-08-03 15:07:04 -0400

    restructured html grouping to fix issues related to event bubbling (the clear button being included in the same Event Target group as the numbers, for example)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const getOperator = document.querySelectorAll("#operator-button");
-const operatorButtonContainer = document.querySelector("#calculator-opeartor-button-container");
-const numberButtonContainer = document.querySelector("#calculator-number-button-container")
+const operatorButtonContainer = document.querySelector("#calculator-operator-button-container");
+const numberButtonContainer = document.querySelector("#calculator-number-button-container");
+const numberButtons = document.querySelector("#number-buttons");
 const displayText = document.querySelector("#display-text");
 const seven = document.querySelector("#seven");
+const clearButton = document.querySelector("#clear-button");
 
 const add = (x, y) => {
     return x + y;
@@ -20,35 +22,43 @@ const divide = (x, y) => {
     return x / y;
 }
 
-let operator; //value of the current operator
+let operator = ""; //value of the current operator
 let firstNumber; //value of the current first number (x)
 let nextNumber; //value of the current second number (y)
 
 const operate = (operator, firstNumber, nextNumber) => {
-    if (operator === "add") {
+    if (operator === "+") {
         return add(firstNumber, nextNumber);
-    } else if (operator === "subtract") {
+    } else if (operator === "-") {
         return subtract(firstNumber, nextNumber);
-    } else if (operator === "multiply") {
+    } else if (operator === "*") {
         return multiply(firstNumber, nextNumber);
-    } else if (operator === "divide") {
+    } else if (operator === "/") {
         return divide(firstNumber, nextNumber);
     }
 };
 
 let numberArray = []; //array used to push the value clicked by the user - that array then has join and parseInt called on it produce an integer value for calculation
 
-numberButtonContainer.addEventListener("click", (e) => {
+numberButtons.addEventListener("click", (e) => {
     numberArray.push(e.target.textContent);
     firstNumber = parseInt(numberArray.join(''));
     displayText.textContent = firstNumber;
 });
 
-operatorButtonContainer.addEventListener("click", (eventTarget) => {
+operatorButtonContainer.addEventListener("click", (e) => {
+    let target = e.target;
 
-    console.log(eventTarget.textContent);
-
+    if (typeof firstNumber === "undefined") {
+        alert("Please Select A Number Before Selecting An Operator");
+    } else {
+        operator = target.textContent;
+        console.log(operator);
+    }
 });
 
-
-
+clearButton.addEventListener("click", () => {
+    numberArray = [0]
+    firstNumber = 0;
+    displayText.textContent = firstNumber;  
+});
